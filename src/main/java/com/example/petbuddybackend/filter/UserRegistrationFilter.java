a31ab655c425fb76e049b5cfc70d69d7b25640e9
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-07-09 14:52:50 +0200

    add filter to detect user interaction

---
+++ src/main/java/com/example/petbuddybackend/filter/UserRegistrationFilter.java
@@ -0,0 +1,29 @@
+package com.example.petbuddybackend.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebMvc
+@Slf4j
+public class UserRegistrationFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        JwtAuthenticationToken token = (JwtAuthenticationToken) request.getUserPrincipal();
+        log.info("Request by user with email: " + token.getTokenAttributes().get("email"));
+
+    }
+
+}
--- src/main/java/com/example/petbuddybackend/utils/keycloak/KeycloakJwtAuthenticationConverter.java
+++ src/main/java/com/example/petbuddybackend/utils/keycloak/KeycloakJwtAuthenticationConverter.java
@@ -1,4 +1,4 @@
-package com.example.petbuddybackend.config;
+package com.example.petbuddybackend.utils.keycloak;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
