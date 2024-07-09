60a08295f998c04cd25a099a6edd572be9a9c11b
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-07-09 10:37:34 +0200

    init

---
+++ src/main/java/com/example/petbuddybackend/PetBuddyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.petbuddybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBuddyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetBuddyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petbuddybackend/PetBuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petbuddybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBuddyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
