7d2e8b7650318f549521d01573194d321e10b024
Karol Pilat <karolpilat59@gmail.com>
2024-07-29 21:47:51 +0200

    Add missing comma

--- src/main/java/com/example/petbuddybackend/config/SecurityConfig.java
+++ src/main/java/com/example/petbuddybackend/config/SecurityConfig.java
@@ -8,10 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -43,7 +41,7 @@ public class SecurityConfig {
                                         "/api-docs.yaml",
                                         "/swagger-ui/**",
                                         "/swagger-ui.html",
-                                        "/api/csrf**"
+                                        "/api/csrf**",
                                         "/api/caretaker"
                                 )
                                     .permitAll()
