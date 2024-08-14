95b30cb3e174b7b0da300b752ba595f36f4bef92
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-08-14 12:30:56 +0200

    fix CaretakerMapperTest

--- src/test/java/com/example/petbuddybackend/testutils/MockUtils.java
+++ src/test/java/com/example/petbuddybackend/testutils/MockUtils.java
@@ -2,9 +2,14 @@ package com.example.petbuddybackend.testutils;
 
 import com.example.petbuddybackend.entity.address.Address;
 import com.example.petbuddybackend.entity.address.Voivodeship;
+import com.example.petbuddybackend.entity.amenity.Amenity;
+import com.example.petbuddybackend.entity.amenity.AnimalAmenity;
 import com.example.petbuddybackend.entity.animal.Animal;
+import com.example.petbuddybackend.entity.animal.AnimalAttribute;
 import com.example.petbuddybackend.entity.offer.Offer;
 import com.example.petbuddybackend.entity.animal.AnimalType;
+import com.example.petbuddybackend.entity.offer.OfferConfiguration;
+import com.example.petbuddybackend.entity.offer.OfferOption;
 import com.example.petbuddybackend.entity.rating.Rating;
 import com.example.petbuddybackend.entity.user.AppUser;
 import com.example.petbuddybackend.entity.user.Caretaker;
@@ -141,6 +146,68 @@ public final class MockUtils {
                 createMockOffer(caretakers.get(0), bird),
                 createMockOffer(caretakers.get(1), dog),
                 createMockOffer(caretakers.get(1), reptile));
+
+    }
+
+    public static Animal createAnimal(String animalType) {
+        return Animal.builder()
+                .animalType(animalType)
+                .build();
+    }
+
+    public static AnimalAttribute createAnimalAttribute(String attributeName, String attributeValue, Animal animal) {
+        return AnimalAttribute.builder()
+                .animal(animal)
+                .attributeName(attributeName)
+                .attributeValue(attributeValue)
+                .build();
+    }
+
+    public static OfferOption createOfferOption(AnimalAttribute animalAttribute) {
+        return OfferOption.builder()
+                .animalAttribute(animalAttribute)
+                .build();
+    }
+
+    public static OfferConfiguration createOfferConfiguration(List<OfferOption> offerOptions) {
+        return OfferConfiguration.builder()
+                .description("description")
+                .dailyPrice(10.0)
+                .offerOptions(offerOptions)
+                .build();
+    }
+
+    public static AnimalAmenity createAnimalAmenity(Animal animal, String amenity) {
+        return AnimalAmenity.builder()
+                .animal(animal)
+                .amenity(Amenity.builder().amenity(amenity).build())
+                .build();
+    }
+
+    public static void createComplexMockOfferForCaretaker(Caretaker caretaker) {
+
+        Animal dog = createAnimal("DOG");
+
+        AnimalAttribute dogAttribute = createAnimalAttribute("SIZE", "BIG", dog);
+        AnimalAttribute dogAttribute2 = createAnimalAttribute("SEX", "MALE", dog);
+
+        List<OfferOption> offerOptions = List.of(
+                createOfferOption(dogAttribute),
+                createOfferOption(dogAttribute2)
+        );
+
+        OfferConfiguration offerConfiguration = createOfferConfiguration(offerOptions);
+        List<AnimalAmenity> animalAmenities = Arrays.asList(
+                createAnimalAmenity(dog, "WALKING"),
+                createAnimalAmenity(dog, "FEEDING")
+        );
+
+        Offer offer = Offer.builder()
+                .animal(dog)
+                .animalAmenities(animalAmenities)
+                .offerConfigurations(List.of(offerConfiguration))
+                .build();
+        caretaker.setOffers(List.of(offer));
 
     }
 }
