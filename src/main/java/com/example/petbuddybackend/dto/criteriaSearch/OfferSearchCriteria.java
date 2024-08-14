b6de7c064cbb84cd347aadf41133ad081e90bc86
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-08-14 13:01:36 +0200

    add more criteria for searching caretaker

--- src/main/java/com/example/petbuddybackend/dto/criteriaSearch/OfferSearchCriteria.java
+++ src/main/java/com/example/petbuddybackend/dto/criteriaSearch/OfferSearchCriteria.java
@@ -12,6 +12,25 @@ public record OfferSearchCriteria(
                 examples = {"DOG", "CAT", "BIRD", "SMALL_PET", "FISH", "REPTILE", "HORSE"},
                 type = "array"
         )
-        Set<String> animalTypes
+        Set<String> animalTypes,
+
+        @Schema(
+                description = "Filters by amenities. Results consists of caretakers that have amenities from the list",
+                examples = {"scratching post", "toys"},
+                type = "array"
+        )
+        Set<String> amenities,
+
+        @Schema(description = "Filters by min price")
+        Double minPrice,
+
+        @Schema(description = "Filters by max price")
+        Double maxPrice,
+
+        @Schema(
+                description = "Filters by attribute values. Results consists of caretakers that have attributes value from the list",
+                type = "array"
+        )
+        Set<Long> animalAttributeIds
 ) {
 }
--- src/main/java/com/example/petbuddybackend/utils/specification/OfferSpecificationUtils.java
+++ src/main/java/com/example/petbuddybackend/utils/specification/OfferSpecificationUtils.java
@@ -1,10 +1,13 @@
 package com.example.petbuddybackend.utils.specification;
 
-import com.example.petbuddybackend.dto.criteriaSearch.CaretakerSearchCriteria;
 import com.example.petbuddybackend.dto.criteriaSearch.OfferSearchCriteria;
+import com.example.petbuddybackend.entity.amenity.Amenity;
+import com.example.petbuddybackend.entity.amenity.AnimalAmenity;
 import com.example.petbuddybackend.entity.animal.Animal;
+import com.example.petbuddybackend.entity.animal.AnimalAttribute;
 import com.example.petbuddybackend.entity.offer.Offer;
-import com.example.petbuddybackend.entity.user.Caretaker;
+import com.example.petbuddybackend.entity.offer.OfferConfiguration;
+import com.example.petbuddybackend.entity.offer.OfferOption;
 import jakarta.persistence.criteria.Join;
 import org.keycloak.common.util.CollectionUtil;
 import org.springframework.data.jpa.domain.Specification;
@@ -15,6 +18,12 @@ public final class OfferSpecificationUtils {
 
     public static final String ANIMAL = "animal";
     public static final String ANIMAL_TYPE = "animalType";
+    public static final String ANIMAl_AMENITIES = "animalAmenities";
+    public static final String AMENITY = "amenity";
+    public static final String OFFER_CONFIGURATIONS = "offerConfigurations";
+    public static final String OFFER_OPTIONS = "offerOptions";
+    public static final String ANIMAL_ATTRIBUTE = "animalAttribute";
+    public static final String ID = "id";
 
     public static Specification<Offer> toSpecification(OfferSearchCriteria filters) {
 
@@ -26,6 +35,22 @@ public final class OfferSpecificationUtils {
             spec = spec.and(animalTypesIn(filters.animalTypes()));
         }
 
+        if(CollectionUtil.isNotEmpty(filters.amenities())) {
+            spec = spec.and(amenitiesIn(filters.amenities()));
+        }
+
+        if(filters.minPrice() != null) {
+            spec = spec.and(minPrice(filters.minPrice()));
+        }
+
+        if(filters.maxPrice() != null) {
+            spec = spec.and(maxPrice(filters.maxPrice()));
+        }
+
+        if(CollectionUtil.isNotEmpty(filters.animalAttributeIds())) {
+            spec = spec.and(animalAttributeIdsIn(filters.animalAttributeIds()));
+        }
+
         return spec;
 
     }
@@ -34,6 +59,31 @@ public final class OfferSpecificationUtils {
         return (root, query, criteriaBuilder) -> {
             Join<Offer, Animal> animalJoin = root.join(ANIMAL);
             return animalJoin.get(ANIMAL_TYPE).in(animalTypes);
+        };
+    }
+
+    private static Specification<Offer> amenitiesIn(Set<String> amenities) {
+        return (root, query, criteriaBuilder) -> {
+            Join<Offer, AnimalAmenity> animalAmenityJoin = root.join(ANIMAl_AMENITIES);
+            Join<AnimalAmenity, Amenity> amenityJoin = animalAmenityJoin.join(AMENITY);
+            return amenityJoin.get(AMENITY).in(amenities);
+        };
+    }
+
+    private static Specification<Offer> minPrice(Double minPrice) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
+    }
+
+    private static Specification<Offer> maxPrice(Double maxPrice) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice);
+    }
+
+    private static Specification<Offer> animalAttributeIdsIn(Set<Long> animalAttributeIds) {
+        return (root, query, criteriaBuilder) -> {
+            Join<Offer, OfferConfiguration> offerOfferConfigurationJoin = root.join(OFFER_CONFIGURATIONS);
+            Join<OfferConfiguration, OfferOption> offerOptionJoin = offerOfferConfigurationJoin.join(OFFER_OPTIONS);
+            Join<OfferOption, AnimalAttribute> animalAttributeJoin = offerOptionJoin.join(ANIMAL_ATTRIBUTE);
+            return animalAttributeJoin.get(ID).in(animalAttributeIds);
         };
     }
 
