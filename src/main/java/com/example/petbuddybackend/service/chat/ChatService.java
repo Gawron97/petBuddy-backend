981fa4a87467c553c834cd50c7d24bf5fe7caed5
lukcyn <lukaszcyndzerr@gmail.com>
2024-08-22 17:25:57 +0200

    Code refactoring

--- src/main/java/com/example/petbuddybackend/service/chat/ChatService.java
+++ src/main/java/com/example/petbuddybackend/service/chat/ChatService.java
@@ -24,13 +24,8 @@ public class ChatService {
     private final ChatMapper chatMapper = ChatMapper.INSTANCE;
 
     public Page<ChatMessageDTO> getChatMessages(Long chatId, String principalEmail, Pageable pageable) {
-        if(!chatRepository.existsById(chatId)) {
-            throw new NotFoundException("Chat with id: " + chatId + " not found");
-        }
-
-        if(!isUserInChat(chatId, principalEmail)) {
-            throw new NotParticipateException("User with email: " + principalEmail + " is not in chat of id " + chatId);
-        }
+        checkChatExistsById(chatId);
+        checkUserParticipatesInChat(chatId, principalEmail);
 
         return chatMessageRepository.findByChatRoom_Id_OrderByCreatedAtDesc(chatId, pageable)
                 .map(chatMapper::mapToChatMessageDTO);
@@ -41,9 +36,21 @@ public class ChatService {
                 .map(message -> convertTimeZone(message, timeZone));
     }
 
+    private void checkChatExistsById(Long chatId) {
+        if(!chatRepository.existsById(chatId)) {
+            throw new NotFoundException("Chat with id: " + chatId + " not found");
+        }
+    }
+
+    private void checkUserParticipatesInChat(Long chatId, String email) {
+        if(!isUserInChat(chatId, email)) {
+            throw new NotParticipateException("User with email: " + email + " is not in chat of id " + chatId);
+        }
+    }
+
     private boolean isUserInChat(Long chatId, String principal) {
-        return chatRepository.caretakerParticipatesInChat(chatId, principal) ||
-                chatRepository.clientParticipatesInChat(chatId, principal);
+        return chatRepository.existsByIdAndClient_Email(chatId, principal) ||
+                chatRepository.existsByIdAndCaretaker_Email(chatId, principal);
     }
 
     private ChatMessageDTO convertTimeZone(ChatMessageDTO message, ZoneId timeZone) {
