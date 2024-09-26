1f12d3af3c55ce4b4641508b1a6fd513a89cc17e
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-09-26 14:15:28 +0200

    add docs and security to getClient endpoint

--- src/main/java/com/example/petbuddybackend/controller/ClientController.java
+++ src/main/java/com/example/petbuddybackend/controller/ClientController.java
@@ -5,7 +5,9 @@ import com.example.petbuddybackend.entity.user.Role;
 import com.example.petbuddybackend.service.user.ClientService;
 import com.example.petbuddybackend.utils.annotation.swaggerdocs.RoleParameter;
 import com.example.petbuddybackend.utils.annotation.validation.AcceptRole;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,9 @@ public class ClientController {
 
     private final ClientService clientService;
 
+    @Operation(summary = "Get client data")
     @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public ClientDTO getClient(Principal principal,
                                @RoleParameter
                                    @AcceptRole(acceptRole = Role.CLIENT)
