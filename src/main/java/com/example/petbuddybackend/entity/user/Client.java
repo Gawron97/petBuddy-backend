2a7658cb5260b2b1f806e4658f6f471d7e5849ac
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-10-07 11:46:09 +0200

    add structure for following caretakers add data generation for following caretakers

--- src/main/java/com/example/petbuddybackend/entity/user/Client.java
+++ src/main/java/com/example/petbuddybackend/entity/user/Client.java
@@ -5,7 +5,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Builder
@@ -25,4 +27,25 @@ public class Client {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "client", fetch = FetchType.LAZY)
     @Builder.Default
     private List<Rating> ratings = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "following_caretakers",
+            joinColumns = @JoinColumn(name = "client_email"),
+            inverseJoinColumns = @JoinColumn(name = "caretaker_email")
+    )
+    @Builder.Default
+    private Set<Caretaker> followingCaretakers = new HashSet<>();
+
+    @PreUpdate
+    public void preUpdate() {
+        assertClientNotFollowingItself();
+    }
+
+    private void assertClientNotFollowingItself() {
+        if (followingCaretakers.stream().anyMatch(caretaker -> caretaker.getEmail().equals(email))) {
+            throw new IllegalStateException("Client cannot follow itself");
+        }
+    }
+
 }
