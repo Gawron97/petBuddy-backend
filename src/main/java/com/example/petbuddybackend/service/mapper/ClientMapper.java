1991030f4eec3e6814fe22b6e8f50d01f00d25fb
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-11-02 18:18:03 +0100

    add client info when retriving ratings of caretaker fix tests

--- src/main/java/com/example/petbuddybackend/service/mapper/ClientMapper.java
+++ src/main/java/com/example/petbuddybackend/service/mapper/ClientMapper.java
@@ -5,7 +5,7 @@ import com.example.petbuddybackend.entity.user.Client;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = {AddressMapper.class, UserMapper.class})
+@Mapper(uses = {UserMapper.class})
 public interface ClientMapper {
 
     ClientMapper INSTANCE = Mappers.getMapper(ClientMapper.class);
--- src/test/java/com/example/petbuddybackend/testutils/mock/MockUserProvider.java
+++ src/test/java/com/example/petbuddybackend/testutils/mock/MockUserProvider.java
@@ -93,6 +93,7 @@ public final class MockUserProvider {
                 .name(name)
                 .surname(surname)
                 .email(email)
+                .profilePicture(createMockPhotoLink())
                 .build();
     }
 
