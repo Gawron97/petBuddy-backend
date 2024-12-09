fa7a1155f102cbb6f4c37904e2637531ec946eae
lukcyn <lukaszcyndzerr@gmail.com>
2024-12-09 17:57:08 +0100

    Allowing user to subscribe but disallow to send messages on block

--- src/main/java/com/example/petbuddybackend/middleware/interceptor/ValidChatRoomAccessInterceptor.java
+++ src/main/java/com/example/petbuddybackend/middleware/interceptor/ValidChatRoomAccessInterceptor.java
@@ -1,10 +1,13 @@
 package com.example.petbuddybackend.middleware.interceptor;
 
 import com.example.petbuddybackend.dto.chat.notification.ChatNotificationBlock;
+import com.example.petbuddybackend.entity.chat.ChatRoom;
 import com.example.petbuddybackend.entity.user.Role;
+import com.example.petbuddybackend.service.block.BlockService;
 import com.example.petbuddybackend.service.block.BlockType;
 import com.example.petbuddybackend.service.chat.ChatService;
 import com.example.petbuddybackend.utils.exception.ApiExceptionResponse;
+import com.example.petbuddybackend.utils.exception.throweable.HttpException;
 import com.example.petbuddybackend.utils.exception.throweable.chat.NotParticipateException;
 import com.example.petbuddybackend.utils.exception.throweable.general.NotFoundException;
 import com.example.petbuddybackend.utils.exception.throweable.user.BlockedException;
@@ -44,6 +47,7 @@ public class ValidChatRoomAccessInterceptor implements ChannelInterceptor, Appli
     private String ROLE_HEADER_NAME;
 
     private final ChatService chatService;
+    private final BlockService blockService;
     private SimpMessagingTemplate simpMessagingTemplate;
     private ApplicationContext applicationContext;
 
@@ -65,50 +69,67 @@ public class ValidChatRoomAccessInterceptor implements ChannelInterceptor, Appli
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
         String destination = accessor.getDestination();
 
-        if(HeaderUtils.destinationStartsWith(URL_CHAT_TOPIC_BASE, destination) && !accessPermitted(accessor, destination)) {
+        if(!HeaderUtils.destinationStartsWith(URL_CHAT_TOPIC_BASE, destination)) {
+            return message;
+        }
+
+        StompCommand command = accessor.getCommand();
+        Role role = Role.valueOf(accessor.getFirstNativeHeader(ROLE_HEADER_NAME));
+        String username = HeaderUtils.getUser(accessor);
+        Long chatId = extractChatId(destination);
+        String sessionId = accessor.getSessionId();
+        ChatRoom chatRoom = chatService.getChatRoomById(chatId);
+
+        if(!accessPermitted(command, chatRoom, username, sessionId, role)) {
             return null;
         }
 
         return message;
     }
 
-    private boolean accessPermitted(StompHeaderAccessor accessor, String destination) {
-        StompCommand command = accessor.getCommand();
-        Role role = Role.valueOf(accessor.getFirstNativeHeader(ROLE_HEADER_NAME));
 
-        if(!StompCommand.SUBSCRIBE.equals(command) && !StompCommand.SEND.equals(command)) {
-            return true;
-        }
-
-        String username = HeaderUtils.getUser(accessor);
-        Long chatId = extractChatId(destination);
-        String sessionId = accessor.getSessionId();
-
-        return validateChatPermission(chatId, username, sessionId, role);
-    }
-
-    private boolean validateChatPermission(Long chatId, String username, String sessionId, Role role) {
-        try {
-            chatService.assertHasAccessToChatRoom(chatId, username, role);
-        } catch(NotParticipateException | NotFoundException | BlockedException e) {
-            log.debug("User {} is not allowed to access chat room {}", username, chatId);
-            log.trace("Exception: {}", e.getMessage());
-            log.trace("Sending exception message to user at destination {}", EXCEPTIONS_PATH);
-            simpMessagingTemplate.convertAndSendToUser(
-                    username,
-                    EXCEPTIONS_PATH,
-                    new ApiExceptionResponse(e, e.getMessage()),
-                    HeaderUtils.createMessageHeadersWithSessionId(sessionId)
-            );
-
-            if(e instanceof BlockedException) {
-                sendBlockNotificationToChatTopic(chatId, username, sessionId);
-            }
-
-            return false;
+    private boolean accessPermitted(StompCommand command, ChatRoom chatRoom, String username, String sessionId, Role role) {
+        if(StompCommand.SUBSCRIBE.equals(command)) {
+            return validateUserParticipatesInChat(chatRoom, username, sessionId, role);
+        } else if(StompCommand.SEND.equals(command)) {
+            return validateUserParticipatesInChat(chatRoom, username, sessionId, role) &&
+                    validateUserNotBlocked(chatRoom, username, sessionId);
         }
 
         return true;
+    }
+
+
+    private boolean validateUserNotBlocked(ChatRoom chatRoom, String username, String sessionId) {
+        try {
+            blockService.assertNotBlockedByAny(chatRoom.getCaretaker().getEmail(), chatRoom.getClient().getEmail());
+            return true;
+        } catch(BlockedException e) {
+            sendBlockNotificationToChatTopic(chatRoom.getId(), username, sessionId);
+            return false;
+        }
+    }
+
+    private boolean validateUserParticipatesInChat(ChatRoom chatRoom, String username, String sessionId, Role role) {
+        try {
+            chatService.assertUserInChat(chatRoom, username, role);
+            return true;
+        } catch(NotParticipateException | NotFoundException e) {
+            sendGeneralExceptionMessage(e, username, chatRoom.getId(), sessionId);
+            return false;
+        }
+    }
+
+    private void sendGeneralExceptionMessage(HttpException e, String username, Long chatId, String sessionId) {
+        log.debug("User {} is not allowed to access chat room {}", username, chatId);
+        log.trace("Exception: {}", e.getMessage());
+        log.trace("Sending exception message to user at destination {}", EXCEPTIONS_PATH);
+        simpMessagingTemplate.convertAndSendToUser(
+                username,
+                EXCEPTIONS_PATH,
+                new ApiExceptionResponse(e, e.getMessage()),
+                HeaderUtils.createMessageHeadersWithSessionId(sessionId)
+        );
     }
 
     private void sendBlockNotificationToChatTopic(Long chatId, String username, String sessionId) {
