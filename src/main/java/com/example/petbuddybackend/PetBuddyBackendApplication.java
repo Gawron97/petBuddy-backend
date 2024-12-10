e723e99e8eb05cb8799ec92b72b438e19e92d5b0
Gawron97 <jakubgawron97@gmail.com>
2024-12-10 11:09:53 +0100

    clean up

--- src/main/java/com/example/petbuddybackend/PetBuddyBackendApplication.java
+++ src/main/java/com/example/petbuddybackend/PetBuddyBackendApplication.java
@@ -1,27 +1,13 @@
 package com.example.petbuddybackend;
 
-import com.example.petbuddybackend.repository.care.CareRepository;
-import com.example.petbuddybackend.repository.user.CaretakerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetBuddyBackendApplication implements CommandLineRunner {
-
-    @Autowired
-    private CareRepository careRepository;
-
-    @Autowired
-    private CaretakerRepository caretakerRepository;
+public class PetBuddyBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PetBuddyBackendApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("sa");
-    }
 }
